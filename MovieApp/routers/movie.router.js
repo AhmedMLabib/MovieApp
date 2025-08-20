6527ed16bc5f66471c6df1f820c9e73b329e1e07
Ahmed Mohamed Labib <166406470+AhmedMLabib@users.noreply.github.com>
2025-08-20 19:54:59 +0300

    --

--- MovieApp/routers/movie.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const movieController = require('../controllers/movies.controller');
-
-router.get('/movies', movieController.getAllMovies);
-router.get('/movies/recommendations', movieController.getRecommendedMovie);
-router.get('/movies/:id', movieController.getMovieById);
-
-module.exports = router;
